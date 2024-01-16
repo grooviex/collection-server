5cd448873162e8af36e6466bacd3b542ac184a4d
Danil Schumin <danil80sch@gmail.com>
2024-01-16 23:37:10 +0100

    Added frontend (again, now final) And made docker work :D

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,6 @@ RUN apt-get update && apt-get upgrade -y
 WORKDIR /usr/src/app
 COPY package.json /usr/src/app
 RUN npm install
-COPY app/ /usr/src/app
+COPY src/ /usr/src/app
 EXPOSE 3000
 CMD npm start
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
+
 /* ===============>
      -- Environment Configuration --
  <=============== */
@@ -23,15 +24,22 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+/* --- Frontend Configuration --- */
+app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "frontend", "views"));
+
+
+/*
+
 /* ===============>
      -- Routing --
  <=============== */
 
 app.get('/', (req, res) => {
-    res.json({status: 'ONLINE'});
+    res.render('index');
 })
 
-app.use('/user',  require('./routes/users'));
+app.use('/user',  require('./backend/routes/users'));
 
 
 const PORT = process.env.PORT || 3000;
