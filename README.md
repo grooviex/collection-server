5d7e34dd3228f75a66afec4a0d55e0e80e298580
Danil Schumin <danil80sch@gmail.com>
2024-07-01 20:56:47 +0200

    starting from scratch
    Signed-off-by: Danil Schumin <danil80sch@gmail.com>

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Grooviex-Server-Collection
 
-The Backend for the Grooviex Project!
-
-> !! **THIS PROJECT WILL BE COMPLETLY REWRITEN, THE ONLY UPDATES WILL BE DATABASE CHANGES** !!
+The New Grooveix Backend written in Nuxt!
--- src/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    port: process.env.PORT,
-    roles: {
-        USER: 'user',
-        ADMIN: 'admin'
-    },
-    jwtSecret: 'db80ed45894fc018c1f5db577887b1c309e0fa0459f655f785d45385750f96c9',
-    jwtExpirationsInSeconds: 60 * 60,
-}
