11284dc4ccc43123cf1911ff7e171d903850cbb1
Danil Schumin <d.schumin@proton.me>
2024-11-12 08:42:03 +0100

    💄🚧 testing frontend design
    Signed-off-by: Danil Schumin <d.schumin@proton.me>

--- src/app.vue
+++ src/app.vue
@@ -10,8 +10,21 @@
 
 <style>
 @import url("~/assets/css/variables/colors.css");
+@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&display=swap');
+
+@font-face {
+  font-family: "Material Symbols Rounded";
+  font-style: normal;
+  font-weight: 400;
+  src: url(https://fonts.gstatic.com/s/materialsymbolsrounded/v194/syl0-zNym6YjUruM-QrEh7-nyTnjDwKNJ_190FjpZIvDmUSVOK7BDB_Qb9vUSzq3wzLK-P0J-V_Zs-QtQth3-jOcbTCVpeRL2w5rwZu2rIelXxc.woff2)
+  format("woff2");
+}
 
 * {
+  font-family: "JetBrains Mono", monospace;
+  font-optical-sizing: auto;
+  font-style: normal;
+
   position: relative;
   padding: 0;
   margin: 0;
